92479990d63806e347a95b24165e4c08d365cfad
Benjamin Worthington <benworthin@icloud.com>
2020-12-13 22:30:07 -0600

    Implemented basic solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int numberOfDigits = 0;
+
+    cout << "Enter how many digits of Pi to generate... > ";
+    cin >> numberOfDigits;
+    cout << endl;
+
+    numberOfDigits++;
+
+    cout << "Pi to " << numberOfDigits - 1 << " decimal places: " << setprecision(numberOfDigits) << M_PI << endl;
+
     return 0;
+
 }
