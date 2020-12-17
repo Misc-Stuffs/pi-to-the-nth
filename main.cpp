f1be4d008748562db1cf4865c96e8393efa54e04
Benjamin Worthington <benworthin@icloud.com>
2020-12-17 16:35:12 -0600

    Implemented loop for limit of digits

--- main.cpp
+++ main.cpp
@@ -3,17 +3,34 @@
 #include <iomanip>
 using namespace std;
 
+int const LIMIT = 20;
+
 int main() {
 
     int numberOfDigits = 0;
 
-    cout << "Enter how many digits of Pi to generate... > ";
-    cin >> numberOfDigits;
-    cout << endl;
+    cout << "Enter how many digits of Pi to generate... (up to 20) > ";
 
-    numberOfDigits++;
+    while (cin >> numberOfDigits) {
 
-    cout << "Pi to " << numberOfDigits - 1 << " decimal places: " << setprecision(numberOfDigits) << M_PI << endl;
+        if (numberOfDigits <= 20) {
+
+            cout << endl;
+
+            numberOfDigits++;
+
+            cout << "Pi to " << numberOfDigits - 1 << " decimal places: " << setprecision(numberOfDigits) << M_PI << endl;
+
+            return 0;
+
+        } else {
+
+            cout << "The limit is " << LIMIT << ". Try again. " << endl;
+            cout << "> ";
+
+        }
+
+    }
 
     return 0;
 
